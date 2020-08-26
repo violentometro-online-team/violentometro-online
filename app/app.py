bce5850699c4b1cf24350b0d8ada08f7e6684589
Iván Galaviz <ivanovishado@gmail.com>
2020-08-26 03:19:51 -0500

    Display model's predictions on app (#7)
    * WIP - Apply model changes to app + setup
    
    * Add model's predictions
    
    The predictions are now being showed on the app as a table, will work on this to show the results in a more graphical way.

--- app/app.py
+++ app/app.py
@@ -1,12 +1,29 @@
 import streamlit as st
+import pickle
+from textops import predict
+
+
+@st.cache(persist=True, allow_output_mutation=True)
+def load_model():
+    return pickle.load(open("final_model.sav", "rb"))
+
+
+@st.cache(persist=True, allow_output_mutation=True)
+def load_CV():
+    return pickle.load(open("final_CV.sav", "rb"))
+
+
+model = load_model()
+CV = load_CV()
 
 st.title("Violentómetro Online")
 st.write(
     "Prototipo para detectar discurso de odio contra las mujeres en medios digitales escritos."
 )
 
-sentence = st.text_input("Ingresa tu mensaje a analizar:")
+sentence = st.text_area("Ingresa tu mensaje a analizar:")
 
 if sentence:
-    st.write("Este mensaje (no) contiene discurso de odio")
-    # st.write(my_model.predict(sentence))
+    prediction, probability = predict(sentence, model, CV)
+    st.write(prediction)
+    st.write(probability)
---
+++ app/textops.py
@@ -0,0 +1,61 @@
+import emoji
+import es_core_news_sm
+import re
+import pandas as pd
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+
+nlp = es_core_news_sm.load()
+
+
+def clean_text(text):
+    """Make text lowercase, remove punctuation and remove words containing numbers."""
+    text = text.lower()
+    text = re.sub(".*?¿", "", text)
+    text = re.sub("[%s]" % re.escape(string.punctuation), "", text)
+    text = re.sub("\w*\d\w*", "", text)
+    text = re.sub("\n", "", text)
+    re.sub("[0-9]+", "", text)
+    return text
+
+
+def delete_stop_words(text):
+    doc = nlp(text)
+    tokens = [token.text for token in doc if not token.is_stop]
+    return " ".join(token for token in tokens)
+
+
+def lemmatize(text):
+    doc = nlp(text)
+    tokens = [token.lemma_ for token in doc]
+    return " ".join(token for token in tokens)
+
+
+def delete_emoji(text):
+    return emoji.get_emoji_regexp().sub(u"", text)
+
+
+def add_features(text):
+    words = text.split()
+    text_len = len(text)
+    text_words_count = len(words)
+    unique_words_count = len(set(words))
+
+    return text_len, text_words_count, unique_words_count
+
+
+def predict(text, model, count_vectorizer):
+    text_cleaned = clean_text(text)
+    without_stop_words = delete_stop_words(text_cleaned)
+    lemmatized = lemmatize(without_stop_words)
+    without_emojis = delete_emoji(lemmatized)
+    text_len, text_words_count, unique_words_count = add_features(without_emojis)
+
+    X_vec = count_vectorizer.transform([without_emojis])
+    vocab = count_vectorizer.get_feature_names()
+    X = pd.DataFrame(X_vec.toarray(), columns=vocab)
+    X["Longitud_Text"] = text_len
+    X["Numero_Palabras_Text"] = text_words_count
+    X["Numero_Palabras_Unicas"] = unique_words_count
+
+    return model.predict(X), model.predict_proba(X)
